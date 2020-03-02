84adb267d2c7062a0f269c1cca7b1600f4cf3f72
Grosty <grostmaster@gmail.com>
2020-03-03 00:00:30 +0200

    init React compl

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import AppHeader from "./components/AppHeader";
 import SearchPanel from "./components/SearchPanel";
 import TodoList from "./components/TodoList";
+import todoData from "./todoData";
 
 
 const App = () => {
+
     return (
         <div>
             <AppHeader />
             <SearchPanel />
-            <TodoList />
+            <TodoList todos={todoData} />
         </div>
     );
 };
